920f1a46014296abd5e3afdd4d9601c557808e30
Asa Dillahunty <stantondillahunty@gmail.com>
2024-07-25 00:13:41 -0500

    :arrow_forward: Small changes to game board when playing
    Game board needs a massive rework. Some components can definitely be reworked here. I think cells/boards in particular could be reworked to display slightly differently when editing versus when playing, but functionally they are incredibly similar and should be at least share some code

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,31 +1,29 @@
 import { useEffect, useState } from 'react';
 import './GameBoard.css';
+import { useGame } from '../util/firebaseAPIs';
 // import { playTrack } from './spotifyAPI';
 
-function GameBoard({ token }) {
+function GameBoard({ token, gameID }) {
 	const [selectedCard, setSelectedCard] = useState({ });
-
-	const getBoard = () => {
-		const board = Array(6).fill().map( () => 
-			Array(5).fill(undefined)
-		);
-
-		board[0][0] = "spotify:track:0aym2LBJBk9DAYuHHutrIl"; // hey jude, beatles
-		board[0][1] = "spotify:track:4xdBrk0nFZaP54vvZj0yx7"; // hot to go, chappell roan
-		board[0][2] = "spotify:track:61KoN6PlBhQD7sivCcf0hA"; // compensating, amine
-
-		return board;
-	}
-	const board = getBoard();
+	const [board, setSelectedBoard] = useState();
+	const { data: gameData, isLoading, isError } = useGame(gameID);
+	
+	useEffect(() => {
+		if (isLoading) return;
+		console.log(gameData);
+		setSelectedBoard(gameData.boards[0]);
+	}, [gameData, isLoading]);
 
 	useEffect(() => {
 		if (selectedCard.i === undefined) {
 			return;
 		}
+		if (!board) return;
+		console.log(board);
 
 		// start the music
-		if (board[selectedCard.i][selectedCard.j]) {
-			console.log(board[selectedCard.i][selectedCard.j]);
+		if (board.grid[selectedCard.i][selectedCard.j]) {
+			console.log(board.grid[selectedCard.i][selectedCard.j]);
 			// learn how to use this:
 			// 		https://developer.spotify.com/documentation/embeds/tutorials/using-the-iframe-api
 			// playTrack(board[selectedCard.i][selectedCard.j], token);
@@ -33,11 +31,21 @@ function GameBoard({ token }) {
 
 	}, [selectedCard, board, token]);
 
+	if (isLoading) {
+		return <p>Loading</p>
+	}
+	else if (isError) {
+		return <p>Some error. Contact site owner.</p>
+	}
+	else if (!board) {
+		return <p>Some error. Contact site owner.</p>
+	}
+
 	return (
 		<div className="game-board">
-			{board.map( (_val, i) =>
+			{Object.keys(board.grid).map( (_val, i) =>
 				<div className='game-col' key={`col-${i}`}>
-					{board[i].map( (_val, j) => 
+					{board.grid[i].map( (_val, j) => 
 						<div 
 							key={`cell-${i}-${j}`} 
 							className='game-cell' 
@@ -53,6 +61,12 @@ function GameBoard({ token }) {
 				<div className='selected-card'>
 					<div className="question-box">
 						<p>Question {selectedCard.i} {selectedCard.j}</p>
+						{ (board.grid[selectedCard.i][selectedCard.j] !== null) &&
+							<>
+								<p>{board.grid[selectedCard.i][selectedCard.j].name}</p>
+								<p>{board.grid[selectedCard.i][selectedCard.j].artists && board.grid[selectedCard.i][selectedCard.j].artists[0].name }</p>
+							</>
+						}
 						<button onClick={() => setSelectedCard({})}>close</button>
 					</div>
 				</div>
